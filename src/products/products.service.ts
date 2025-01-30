50fc3c8ebe60fee902fb048bc529b5f737445733
rodrigo <javierluis15975@hotmail.com>
2025-01-29 22:36:12 -0500

    update products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,16 +5,24 @@ import {
   Logger,
 } from '@nestjs/common'
 import Stripe from 'stripe'
+import { configRabbit } from './common/configRabbit'
 
 @Injectable()
 export class ProductsService {
   private readonly logger: Logger = new Logger(ProductsService.name)
   constructor(private readonly amqConnection: AmqpConnection) {}
-  public async decrementStock(metadata: Stripe.Metadata) {
+  public decrementStock(metadata: Stripe.Metadata) {
+    console.log({
+      metadata,
+    })
+
     try {
-      console.log({
-        decrement: metadata,
-      })
+      this.amqConnection.publish(
+        configRabbit.EXCHANGE_NAME_DECREMENTE_STOCK,
+        configRabbit.ROUTING_KEY_DECREMENTE_STOCK,
+        metadata,
+      )
+      this.logger.verbose('Send decremente stock in product DB-WRITE-READ...')
     } catch (error) {
       this.logger.error('Error send decremente stock in product DB-WRITE-READ')
       throw new InternalServerErrorException({
