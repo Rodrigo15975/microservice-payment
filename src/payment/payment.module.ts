0b99dcedb6d86b9a04b1f84c12341e842d7a8be2
rodrigo <javierluis15975@hotmail.com>
2025-01-31 15:16:03 -0500

    update payment.module.ts

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -2,11 +2,20 @@ import { StripeModule } from '@golevelup/nestjs-stripe'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { PaymentService } from './payment.service'
-// import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq'
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq'
+import { configExchange, configQueue } from './common/config-rabbit'
 // import { configExchange, configQueue } from './common/config-rabbit'
 
 @Module({
   imports: [
+    RabbitMQModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow('RABBITMQ_URL'),
+        exchanges: configExchange,
+        queues: configQueue,
+      }),
+      inject: [ConfigService],
+    }),
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
     }),
