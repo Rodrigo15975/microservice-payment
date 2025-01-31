4195340c15f9450f4382f03765768422786d17b3
rodrigo <javierluis15975@hotmail.com>
2025-01-31 15:16:24 -0500

    update config-rabbit.ts

--- src/payment/common/config-rabbit.ts
+++ src/payment/common/config-rabbit.ts
@@ -7,6 +7,10 @@ export const configRabbit = {
   ROUTING_EXCHANGE_CREATE_PAYMENT: 'payment.create.payment',
   ROUTING_ROUTINGKEY_CREATE_PAYMENT: 'payment.create.payment',
   ROUTING_QUEUE_CREATE_PAYMENT: 'payment.create.payment',
+
+  ROUTING_EXCHANGE_GET_ALL_ORDERS: 'client.get.all.order',
+  ROUTING_ROUTINGKEY_GET_ALL_ORDERS: 'client.get.all.order',
+  ROUTING_QUEUE_GET_ALL_ORDERS: 'client.get.all.order',
 }
 export const configQueue: RabbitMQQueueConfig[] = [
   {
@@ -17,6 +21,17 @@ export const configQueue: RabbitMQQueueConfig[] = [
       durable: true,
     },
   },
+  {
+    name: configRabbit.ROUTING_QUEUE_GET_ALL_ORDERS,
+    routingKey: configRabbit.ROUTING_ROUTINGKEY_GET_ALL_ORDERS,
+    exchange: configRabbit.ROUTING_EXCHANGE_GET_ALL_ORDERS,
+    options: {
+      durable: true,
+      // expires: 60000,
+      // messageTtl: 30000, // Los mensajes expiran si no son consumidos en el tiempo definido.
+      // autoDelete: true, //Borra la cola cuando ya no hay consumidores activos.
+    },
+  },
 ]
 export const configExchange: RabbitMQExchangeConfig[] = [
   {
@@ -24,6 +39,16 @@ export const configExchange: RabbitMQExchangeConfig[] = [
     type: 'direct',
     options: {
       durable: true,
+    },
+  },
+  {
+    name: configRabbit.ROUTING_EXCHANGE_GET_ALL_ORDERS,
+    type: 'direct',
+    options: {
+      durable: true,
+      // expires: 60000,
+      // messageTtl: 30000, // Los mensajes expiran si no son consumidos en el tiempo definido.
+      // autoDelete: true, //Borra la cola cuando ya no hay consumidores activos.
     },
   },
 ]
